41c9a52ecd90461fe1d63612e31885a65927343e
Unknown <jeffreysapia@gmail.com>
2018-07-25 23:39:40 -0700

    Update

--- src/main/java/com/convrt/stream/ConvrtStreamApplication.java
+++ src/main/java/com/convrt/stream/ConvrtStreamApplication.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ConvrtStreamApplication {
 
     public static void main(String[] args) {
-        System.getProperties().put("spring.http.multipart.max-file-size", "-1");
-        System.getProperties().put("spring.http.multipart.max-request-size", "-1");
         SpringApplication.run(ConvrtStreamApplication.class, args);
     }
 
