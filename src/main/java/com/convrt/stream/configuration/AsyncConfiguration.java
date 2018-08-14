cfb4fdc2eb43eac44bb6220276d8a970109b7ac5
Unknown <jeffreysapia@gmail.com>
2018-08-14 16:02:49 -0700

    Update async config

--- src/main/java/com/convrt/stream/configuration/AsyncConfiguration.java
+++ src/main/java/com/convrt/stream/configuration/AsyncConfiguration.java
@@ -1,22 +1,13 @@
 package com.convrt.stream.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.async.CallableProcessingInterceptor;
-import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.concurrent.Callable;
 
 @Slf4j
 @EnableAsync
@@ -31,7 +22,13 @@ public class AsyncConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public AsyncTaskExecutor asyncTaskExecutor() {
-        return new SimpleAsyncTaskExecutor("async");
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setAllowCoreThreadTimeOut(true);
+        executor.setCorePoolSize(50);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("async-stream");
+        return executor;
     }
 
 
